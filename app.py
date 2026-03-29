e51fe7e804007abde5c707d316fb533a8e2b5d9c
kmishra207192 <kuldeep.mishra207192@gmail.com>
2026-03-29 21:15:35 +0530

    fix port and python version

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from api.index import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
